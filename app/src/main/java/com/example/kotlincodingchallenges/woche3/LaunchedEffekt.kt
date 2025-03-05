f4c415e1fe9562af2968b6887297334fa4460ccd
brewdiHQ <stephan.sattel@icloud.com>
2025-03-05 12:05:48 +0100

    Add changes

--- app/src/main/java/com/example/kotlincodingchallenges/woche3/LaunchedEffekt.kt
+++ app/src/main/java/com/example/kotlincodingchallenges/woche3/LaunchedEffekt.kt
@@ -2,6 +2,7 @@ package com.example.kotlincodingchallenges.woche3
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -17,23 +18,38 @@ import androidx.compose.ui.unit.sp
 import kotlinx.coroutines.delay
 
 @Composable
-fun LaunchedEffekt() {
+ fun LaunchedEffekt() {
     var delayMessage by remember { mutableStateOf("") }
+    var resetMessage by remember { mutableStateOf(false) }
 
-    LaunchedEffect(delayMessage) {
-        delay(6000)
-        delayMessage = "Lorem Ipsum"
+    LaunchedEffect(resetMessage) {
+        if (resetMessage) {
+            delay(6000)
+            delayMessage = "Lorem Ipsum"
+            resetMessage = false
+        }
     }
 
     Box(
         modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center) {
+        contentAlignment = Alignment.Center
+    ) {
 
         Text(
             text = delayMessage,
             fontSize = 36.sp,
             color = Color(0xFFFFA500)
         )
+
+        Button(
+            onClick = {
+                delayMessage = ""
+                resetMessage = true
+            },
+            modifier = Modifier.align(Alignment.TopCenter)
+        ) {
+            Text(text = "Reset")
+        }
     }
 }
 
